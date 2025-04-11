e12e2e1ce4bbe224ea19c88f6070a37e2908f2df
Dayem Siddiqui <dayem.siddiqui@personio.de>
2025-04-11 23:57:32 +0200

    Fix the reading advert

--- services/reading_exam_service.py
+++ services/reading_exam_service.py
@@ -2,14 +2,28 @@ from workflows.generate_reading_adverts import ReadingAdvertExamWorkflow, Readin
 from pydantic import BaseModel
 from typing import List
 import random
+import hashlib
+class Advert(BaseModel):
+    id: str
+    text: str   
+
+class AdvertQuestion(BaseModel):
+    id: str
+    question: str
+    correct_advert_id: str
+    explanation: str
 
 class ReadingAdvertExamResult(BaseModel):
-    questions: List[ReadingAdvert]
-    adverts: List[str]
+    questions: List[AdvertQuestion]
+    adverts: List[Advert]
+
 
 class ReadingExamService:
     def __init__(self):
         self.workflow = ReadingAdvertExamWorkflow()
+
+    def to_hash_id(self, text: str) -> str:
+        return hashlib.sha256(text.encode()).hexdigest()
 
     async def get_advert_section(self) -> ReadingAdvertExamResult:
         exam: ReadingAdvertExam = await self.workflow.generate_exam()
@@ -23,9 +37,15 @@ class ReadingExamService:
         ## Combine the correct answer and the two wrong answers
         adverts = correct_answers + wrong_answers
 
+        ## Create the adverts
+        adverts = [Advert(id=self.to_hash_id(advert), text=advert) for advert in adverts]
+
         ## Randomly shuffle the adverts
         random.shuffle(adverts)
 
+        ## Create the advert questions
+        advert_questions = [AdvertQuestion(id=self.to_hash_id(question.question), question=question.question, correct_advert_id=self.to_hash_id(question.correct_advert), explanation=question.explanation) for question in questions]
+
         ## Create the exam result
-        result = ReadingAdvertExamResult(questions=questions, adverts=adverts)
+        result = ReadingAdvertExamResult(questions=advert_questions, adverts=adverts)
         return result
